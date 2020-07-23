bec94fe96bf34e74ffcb1b1e1f9279e266c00db5
Alastair Melville (SI) <alastair.melville@m-si-amelville.local>
2020-07-23 09:33:29 +0100

    first commit

---
+++ recurrent/shape_generation/main.py
@@ -0,0 +1,364 @@
+
+
+import numpy as np
+import random
+
+from utils import *
+
+class S():
+    s_newline_id : int = ord('\n')
+    s_vocab_size : int = 0
+    s_data : list = []
+
+def clip(gradients: dict, maxValue: int) -> dict:
+    '''
+    Clips the gradients' values between minimum and maximum.
+    
+    Arguments:
+    gradients -- a dictionary containing the gradients "dWaa", "dWax", "dWya", "db", "dby"
+    maxValue -- everything above this number is set to this number, and everything less than -maxValue is set to -maxValue
+    
+    Returns: 
+    gradients -- a dictionary with the clipped gradients.
+    '''
+    
+    dWaa, dWax, dWya, db, dby = gradients['dWaa'], gradients['dWax'], gradients['dWya'], gradients['db'], gradients['dby']
+   
+    ### START CODE HERE ###
+    # clip to mitigate exploding gradients, loop over [dWax, dWaa, dWya, db, dby]. (≈2 lines)
+    for gradient in [dWaa, dWax, dWya, db, dby]:
+        np.clip(gradient, -maxValue, maxValue, out=gradient)
+    ### END CODE HERE ###
+    
+    gradients = {"dWaa": dWaa, "dWax": dWax, "dWya": dWya, "db": db, "dby": dby}
+    
+    return gradients
+
+def sample(parameters : dict) -> list:
+    """
+    Sample a sequence of characters according to a sequence of probability distributions output of the RNN
+
+    Arguments:
+    parameters -- python dictionary containing the parameters Waa, Wax, Wya, by, and b.
+
+    Returns:
+    indices -- a list of length n containing the indices of the sampled characters.
+    """
+    
+    # Retrieve parameters and relevant shapes from "parameters" dictionary
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    #vocab_size = by.shape[0]
+    n_a = Waa.shape[1]
+    
+    ### START CODE HERE ###
+    # Step 1: Create the a zero vector x that can be used as the one-hot vector 
+    # representing the first character (initializing the sequence generation). (≈1 line)
+    x = np.zeros(shape=(S.s_vocab_size, 1))
+    # Step 1': Initialize a_prev as zeros (≈1 line)
+    a_prev = np.zeros(shape=(n_a, 1))
+    
+    # Create an empty list of indices, this is the list which will contain the list of indices of the characters to generate (≈1 line)
+    indices = []
+    
+    # idx is the index of the one-hot vector x that is set to 1
+    # All other positions in x are zero.
+    # We will initialize idx to -1
+    idx = -1 
+    
+    # Loop over time-steps t. At each time-step:
+    # sample a character from a probability distribution 
+    # and append its index (`idx`) to the list "indices". 
+    # We'll stop if we reach 50 characters 
+    # (which should be very unlikely with a well trained model).
+    # Setting the maximum number of characters helps with debugging and prevents infinite loops. 
+    counter = 0
+    newline_character = S.s_newline_id
+    
+    while (idx != newline_character and counter != 50):
+        
+        # Step 2: Forward propagate x using the equations (1), (2) and (3)
+        a = np.tanh(np.dot(Waa, a_prev) + np.dot(Wax, x) + b)
+        z = np.dot(Wya, a) + by
+        y = softmax(z)
+                
+        # Step 3: Sample the index of a character within the vocabulary from the probability distribution y
+        # (see additional hints above)
+        idx = np.random.choice(range(S.s_vocab_size), p = y.ravel())
+
+        # Append the index to "indices"
+        indices.append(idx)
+        
+        # Step 4: Overwrite the input x with one that corresponds to the sampled index `idx`.
+        # (see additional hints above)
+        x = np.zeros(shape=(S.s_vocab_size, 1))
+        x[idx] = 1
+        
+        # Update "a_prev" to be "a"
+        a_prev = a
+        
+        counter +=1
+        
+    ### END CODE HERE ###
+
+    if (counter == 50):
+        indices.append(S.s_newline_id)
+    
+    return indices
+
+def optimize(X, Y, a_prev, parameters, learning_rate : float = 0.01):
+    """
+    Execute one step of the optimization to train the model.
+    
+    Arguments:
+    X -- list of integers, where each integer is a number that maps to a character in the vocabulary.
+    Y -- list of integers, exactly the same as X but shifted one index to the left.
+    a_prev -- previous hidden state.
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    learning_rate -- learning rate for the model.
+    
+    Returns:
+    loss -- value of the loss function (cross-entropy)
+    gradients -- python dictionary containing:
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dWya -- Gradients of hidden-to-output weights, of shape (n_y, n_a)
+                        db -- Gradients of bias vector, of shape (n_a, 1)
+                        dby -- Gradients of output bias vector, of shape (n_y, 1)
+    a[len(X)-1] -- the last hidden state, of shape (n_a, 1)
+    """
+    
+    ### START CODE HERE ###
+    
+    # Forward propagate through time (≈1 line)
+    loss, cache = rnn_forward(X, Y, a_prev, parameters) # todo I added the param 11
+    
+    # Backpropagate through time (≈1 line)
+    gradients, a = rnn_backward(X, Y, parameters, cache)
+    
+    # Clip your gradients between -5 (min) and 5 (max) (≈1 line)
+    gradients = clip(gradients, 5)
+    
+    # Update parameters (≈1 line)
+    parameters = update_parameters(parameters, gradients, learning_rate)
+    
+    ### END CODE HERE ###
+    
+    return loss, gradients, a[len(X)-1]
+
+def smooth(loss, cur_loss):
+    return loss * 0.999 + cur_loss * 0.001
+
+def get_initial_loss(seq_length : int):
+    return -np.log(1.0/S.s_vocab_size)*seq_length
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def initialize_parameters(n_a, n_x, n_y):
+    """
+    Initialize parameters with small random values
+    
+    Returns:
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    """
+    np.random.seed(1)
+    Wax = np.random.randn(n_a, n_x)*0.01 # input to hidden
+    Waa = np.random.randn(n_a, n_a)*0.01 # hidden to hidden
+    Wya = np.random.randn(n_y, n_a)*0.01 # hidden to output
+    b = np.zeros((n_a, 1)) # hidden bias
+    by = np.zeros((n_y, 1)) # output bias
+    
+    parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b,"by": by}
+    
+    return parameters
+
+def rnn_step_forward(parameters, a_prev, x):
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b) # hidden state
+    p_t = softmax(np.dot(Wya, a_next) + by) # unnormalized log probabilities for next chars # probabilities for next chars 
+    
+    return a_next, p_t
+
+def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
+    gradients['dWya'] += np.dot(dy, a.T)
+    gradients['dby'] += dy
+    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next'] # backprop into h
+    daraw = (1 - a * a) * da # backprop through tanh nonlinearity
+    gradients['db'] += daraw
+    gradients['dWax'] += np.dot(daraw, x.T)
+    gradients['dWaa'] += np.dot(daraw, a_prev.T)
+    gradients['da_next'] = np.dot(parameters['Waa'].T, daraw)
+    return gradients
+
+def update_parameters(parameters, gradients, lr):
+
+    parameters['Wax'] += -lr * gradients['dWax']
+    parameters['Waa'] += -lr * gradients['dWaa']
+    parameters['Wya'] += -lr * gradients['dWya']
+    parameters['b']  += -lr * gradients['db']
+    parameters['by']  += -lr * gradients['dby']
+    return parameters
+
+def rnn_forward(X, Y, a0, parameters):
+    
+    # Initialize x, a and y_hat as empty dictionaries
+    x, a, y_hat = {}, {}, {}
+    
+    a[-1] = np.copy(a0)
+    
+    # initialize your loss to 0
+    loss = 0
+    
+    for t in range(len(X)):
+        
+        # Set x[t] to be the one-hot vector representation of the t'th character in X.
+        # if X[t] == None, we just have x[t]=0. This is used to set the input for the first timestep to the zero vector. 
+        x[t] = np.zeros((S.s_vocab_size,1)) 
+        if (X[t] != None):
+            x[t][X[t]] = 1
+        
+        # Run one step forward of the RNN
+        a[t], y_hat[t] = rnn_step_forward(parameters, a[t-1], x[t])
+
+        # Update the loss by substracting the cross-entropy term of this time-step from it.
+        loss -= np.log(y_hat[t][Y[t],0])
+        
+    cache = (y_hat, a, x)
+        
+    return loss, cache
+
+def rnn_backward(X, Y, parameters, cache):
+    # Initialize gradients as an empty dictionary
+    gradients = {}
+    
+    # Retrieve from cache and parameters
+    (y_hat, a, x) = cache
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    
+    # each one should be initialized to zeros of the same dimension as its corresponding parameter
+    gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
+    gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
+    gradients['da_next'] = np.zeros_like(a[0])
+    
+    ### START CODE HERE ###
+    # Backpropagate through time
+    for t in reversed(range(len(X))):
+        dy = np.copy(y_hat[t])
+        dy[Y[t]] -= 1
+        gradients = rnn_step_backward(dy, gradients, parameters, x[t], a[t], a[t-1])
+    ### END CODE HERE ###
+    
+    return gradients, a
+
+
+def model(data, num_iterations = 4500, n_a = 50, num_samples = 7, verbose = False):
+    """
+    Trains the model and generates dinosaur names. 
+    
+    Arguments:
+    data -- text corpus
+    num_iterations -- number of iterations to train the model for
+    n_a -- number of units of the RNN cell
+    num_samples -- number of dinosaur names you want to sample at each iteration. 
+    vocab_size -- number of unique characters found in the text (size of the vocabulary)
+    
+    Returns:
+    parameters -- learned parameters
+    """
+    
+    # Retrieve n_x and n_y from vocab_size
+    n_x, n_y = S.s_vocab_size, S.s_vocab_size
+    # Initialize parameters
+    parameters = initialize_parameters(n_a, n_x, n_y)
+    
+    # Initialize loss (this is required because we want to smooth our loss)
+    loss = get_initial_loss(num_samples)
+
+    with open("data.txt") as f:
+        examples = f.readlines()
+    examples = [line.lower().strip() for line in examples]
+    #examples = [item for sublist in data_matrix for item in sublist]
+    
+    #print("Unshuffled: ", examples[:10])
+    # Shuffle list of all dinosaur names
+    np.random.seed(0)
+    np.random.shuffle(examples)
+    #print("Shuffled: ", examples[:10])
+
+    # Initialize the hidden state of your LSTM
+    a_prev = np.zeros((n_a, 1))
+    
+    # Optimization loop
+    for j in range(num_iterations):
+        
+        ### START CODE HERE ###
+        
+        # Set the index `idx` (see instructions above)
+        idx = j % len(examples)
+        
+        X = [None] + [element_to_idx(ch) for ch in examples[idx]] #inputs to the RNN
+        Y = X[1:] + [S.s_newline_id] #the targets it should be outputting
+
+        # Perform one optimization step: Forward-prop -> Backward-prop -> Clip -> Update parameters
+        # Choose a learning rate of 0.01
+        curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters, learning_rate = 0.01)
+        
+        ### END CODE HERE ###
+        
+        # debug statements to aid in correctly forming X, Y
+        if verbose and j in [0, len(examples) -1, len(examples)]:
+            print("j = " , j, "idx = ", idx,) 
+        if verbose and j in [0]:
+            #print("single_example =", single_example)
+            #print("single_example_chars", single_example_chars)
+            #print("single_example_ix", single_example_ix)
+            print(" X = ", X, "\n", "Y =       ", Y, "\n")
+        
+        # Use a latency trick to keep the loss smooth. It happens here to accelerate the training.
+        loss = smooth(loss, curr_loss)
+
+        # Every 2000 Iteration, generate "n" characters thanks to sample() to check if the model is learning properly
+        if j % 2000 == 0:
+            
+            print('Iteration: %d, Loss: %f' % (j, loss) + '\n')
+            
+            # The number of dinosaur names to print
+            seed = 0
+            for name in range(num_samples):
+                
+                # Sample indices and print them
+                sampled_indices = sample(parameters)
+                txt = ''.join(chr(ix) for ix in sampled_indices)
+                txt = txt[0].upper() + txt[1:]  # capitalize first character 
+                print ('%s' % (txt, ), end=',')
+                
+                seed += 1  # To get the same result (for grading purposes), increment the seed by one. 
+      
+            print('\n')
+        
+    return parameters
+
+def main():
+    with open("data.txt", "r") as data_file:
+        lines : list = [line.strip().split(',') + ['\n'] for line in data_file]
+        data : list = [item for sublist in lines for item in sublist]
+        chars : list = sorted(set(data))
+        data_size, S.s_vocab_size = len(data), len(chars)
+        print('There are %d total characters and %d unique characters in your data.' % (data_size, S.s_vocab_size))
+        #s_newline_id = get_newline_id()
+
+        parameters = model(data, verbose = True)
+
+if __name__=="__main__":
+    main()
---
+++ recurrent/shape_generation/utils.py
@@ -0,0 +1,12 @@
+
+
+def idx_to_element(index : int):
+    return chr(index)
+
+def element_to_idx(element) -> int:
+    return ord(element)
+
+def get_newline_id() -> int:
+    return ord('\n')
+
+#print(element_to_idx('\n'))
